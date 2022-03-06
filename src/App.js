197c7266fa75185f84d0436b18901e7df3f1a84d
Rafael Estrada <rafaelestrada@comcast.net>
2022-03-05 21:24:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import searchIcon from './search.svg';
 
-function App() {
+
+const API_URL ='http://www.omdbapi.com/?i=tt3896198&apikey=c3d2f9e5'
+
+
+const movie1={
+    
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    
+}
+
+
+const App = () => {
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${encodeURIComponent(title)}`);
+    const data = await response.json();
+    console.log(data.Search);
+}
+
+    useEffect(() => {
+        searchMovies('batman');
+
+
+    }, []);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+        <h1>MovieLand</h1>
+        <div className="search">
+            <input placeholder='Search for movies'
+            value='Superman'
+            onChange={(e) => searchMovies(e.target.value)}
+            />
+            <img src={searchIcon} 
+            alt="search"
+            onClick={() => searchMovies('batman')}
+            />
+            </div>
+        </div>
   );
 }
 
 export default App;
+
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=c3d2f9e5
