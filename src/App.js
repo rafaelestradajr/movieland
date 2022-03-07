5ea4f2c5c043e9e4378f7f92a9b3a5eef61b6f53
Rafael Estrada <rafaelestrada@comcast.net>
2022-03-06 19:40:45 -0800

    added env file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL ="http://www.omdbapi.com/?i=tt3896198&apikey=c3d2f9e5"
+const API_URL =
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
